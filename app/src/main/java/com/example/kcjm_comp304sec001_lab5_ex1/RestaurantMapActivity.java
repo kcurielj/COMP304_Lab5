830545d2f2c8cf414de742296c5c1a4e832d908b
kcurielj <kcurielj@my.centennialcollege.ca>
2022-11-22 18:45:44 -0500

    Receive Restaurant name on activity
    Receive Restaurant name on activity

--- app/src/main/java/com/example/kcjm_comp304sec001_lab5_ex1/RestaurantMapActivity.java
+++ app/src/main/java/com/example/kcjm_comp304sec001_lab5_ex1/RestaurantMapActivity.java
@@ -6,9 +6,21 @@ import android.os.Bundle;
 
 public class RestaurantMapActivity extends AppCompatActivity {
 
+    String restaurantName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_map);
+
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras == null) {
+                restaurantName = null;
+            } else {
+                restaurantName = extras.getString("RestaurantName");
+            }
+        }
+
+
     }
 }
